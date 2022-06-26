d5bab3784cf3b10b02eaf2f6638addfef165143b
Andres Cuero <andresfelipecuero@gmail.com>
2022-06-26 19:23:20 -0400

    database movie mongo

--- server/config/db.js
+++ server/config/db.js
@@ -1,6 +1,7 @@
 module.exports = {
   //local MongoDB deployment ->
   //Mongo collection for movies database
-  "URI": "mongodb+srv://user:user@cluster0.dpbx2.mongodb.net/?retryWrites=true&w=majority" 
+  //"URI": "mongodb+srv://user:user@cluster0.dpbx2.mongodb.net/?retryWrites=true&w=majority" 
+  "URI": "mongodb+srv://user:user@cluster0.dpbx2.mongodb.net/movies?retryWrites=true&w=majority" 
   /*ADD your cloud URL Here, database Name should be midterm229, and collection name should be the one in the model*/
 };
